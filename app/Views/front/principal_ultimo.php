2938f4325d27a4ebefc4abb51cf36bb8d283cc4d
lourdes <lourdes.ac3012@gmail.com>
2023-10-08 15:59:42 -0300

    creacion repositorio git y primer commit

---
+++ app/Views/front/principal_ultimo.php
@@ -0,0 +1,37 @@
+<div class="container">
+<section>
+    <p class="center-bold-italic">
+      Somos una empresa que se dedica a la venta de relojes de mesa y pared con diseños únicos e innovadores.
+      Utilizamos materia prima de calidad. Elaboramos productos a la medida del cliente.
+    </p>
+    <p>
+      Agregamos otro párrafo para demostrar cómo se aplica el estilo a múltiples párrafos.
+    </p>
+    <img src="./img/reloj.jpg" height="250" width="500" alt="MDN reloj" />
+</section>
+
+<!--Carrusel -->
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/img/3.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/reloj2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/reloj1.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<!-- Fin carrusel -->
+</div>
